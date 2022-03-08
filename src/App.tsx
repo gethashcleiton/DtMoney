0f5c560661daa127c8dd96d2b9360d69d061733d
gethashcleiton <cleiton.mariano@gethashs.onmicrosoft.com>
2022-03-08 16:28:43 -0300

    create modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewtransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+  function handleCloseNewtransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewtransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewtransactionModal}
+      />
+
       <GlobalStyles />
     </>
   );
